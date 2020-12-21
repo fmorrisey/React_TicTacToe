cb257c399ff7c62cec15e93cf38bc33ab4eda013
Forrest Morrisey <forrestmorrisey@gmail.com>
2020-12-21 13:33:20 -0600

    Xs are now Os logic

--- src/game/game.js
+++ src/game/game.js
@@ -1,36 +1,46 @@
 import React, { Component } from 'react';
 import './game.css';
 
-//Renders a single square button
-class Square extends Component {
-  render() {
-    return (
+// Renders a single square button
+// This is a controlled component with it's state
+// controlled by the parent component of the Board.
+function Square(props) {
+  return (
       <button className="square"
-        onClick={() => this.props.onClick()}
+        onClick={props.onClick}
       >
-        {this.props.value}
+        {props.value}
       </button>
     );
-  } 
-}
- 
+  }
+
 // Renders 9 square components  
 class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: false,
     };
   }
 
+  // These two components give control over the square by the board
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      squares : squares,
+      xIsNext: !this.state.xIsNext,
+     });
+  }
 
   renderSquare(i) { //[pass as prop called value to the square]
     // this value is passed from the parent board to the child square
-    return <Square value={this.state.squares[i]} onClick={() =>this.handleClick(i)} />;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
